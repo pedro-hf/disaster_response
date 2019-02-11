4a6598f0c947d9e5c1acc0ee974a5702679bf46c
Pedro Hernandez Fraczek <pedro.hf86@gmail.com>
2019-02-11 21:20:42 +0100

    Add two more plots to the apps index page

--- app/run.py
+++ app/run.py
@@ -2,18 +2,24 @@ import json
 import plotly
 import pandas as pd
 
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-
 from flask import Flask
 from flask import render_template, request
 from plotly.graph_objs import Bar
+from plotly.graph_objs import Histogram
 from sqlalchemy import create_engine
 from joblib import load
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
 
 app = Flask(__name__)
 
+
 def tokenize(text):
+    stop_words = set(stopwords.words('english'))
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
 
@@ -21,7 +27,7 @@ def tokenize(text):
     for tok in tokens:
         clean_tok = lemmatizer.lemmatize(tok).lower().strip()
         clean_tokens.append(clean_tok)
-
+    clean_tokens = [w for w in clean_tokens if not w in stop_words]
     return clean_tokens
 
 # load data
@@ -38,12 +44,20 @@ model = load("../models/disaster_response.joblib")
 def index():
     
     # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    
+
+    message_lengths = df.message.str.len()
+    message_lengths = message_lengths[message_lengths < 600]
+
+    list_words = []
+    for text in df.message:
+        list_words += tokenize(text)
+    top_words = pd.Series(list_words).value_counts()
+    top_words = top_words[top_words.index.str.len() > 2].iloc[:10]
+    top_words_values = top_words.values
+    top_words = top_words.index
     # create visuals
-    # TODO: Below is an example - modify to create your own visuals
     graphs = [
         {
             'data': [
@@ -60,6 +74,42 @@ def index():
                 },
                 'xaxis': {
                     'title': "Genre"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=top_words,
+                    y=top_words_values
+                )
+            ],
+
+            'layout': {
+                'title': 'Top words in corpus',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Words"
+                }
+            }
+        },
+        {
+            'data': [
+                Histogram(
+                    x=message_lengths,
+                    nbinsx=50
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message length',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Number of Characters"
                 }
             }
         }
